564046bab84b49a81c54dc592860b051e28e8c70
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-05-23 11:05:57 +0600

    Basic starter kit

---
+++ build/lch-starter/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e2279c2f0f8ac389840d');
---
+++ build/lch-starter/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'c03272656e4b497107b2');
